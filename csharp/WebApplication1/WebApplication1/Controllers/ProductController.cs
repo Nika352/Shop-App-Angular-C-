9cf2a3e3d46ae05845ec3446294c8ba530030208
Nika Arabidze <102316119+Nika352@users.noreply.github.com>
2023-04-12 02:48:26 +0400

    Add files via upload

---
+++ csharp/WebApplication1/WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Cors;
+
+namespace WebApplication1.Controllers
+{
+    
+    public class ProductController : Controller
+    {
+
+        SqlConnection conn = new SqlConnection("Server=DESKTOP-IJL7MF3; Database=Products; " +
+            "Trusted_Connection=true; ");
+
+
+        //returns list of products from all the values on the table.
+        [HttpGet("GetProducts")]
+        public List<Product> getAllProducts()
+        {
+
+            List<Product> result = new List<Product>();
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM [Products].[dbo].[ProductsTable]", conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+
+                int id = Int32.Parse((dt.Rows[i]["id"].ToString()));
+                string title = (dt.Rows[i]["title"].ToString());
+                double price = double.Parse((dt.Rows[i]["price"].ToString()));
+                string description = (dt.Rows[i]["description"].ToString());
+                string category = (dt.Rows[i]["category"].ToString());
+                string image = (dt.Rows[i]["image"].ToString());
+
+                result.Add(new Product(id, title, price, description, category, image));
+            }
+
+            return result;
+        }
+
+        [HttpGet("GetProductById")]
+        public Product getById(int id)
+        {
+            SqlCommand command = new SqlCommand("getById", conn);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@id", id);
+
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = command;
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            string title = (dt.Rows[0]["title"].ToString());
+            double price = double.Parse((dt.Rows[0]["price"].ToString()));
+            string description = (dt.Rows[0]["description"].ToString());
+            string category = (dt.Rows[0]["category"].ToString());
+            string image = (dt.Rows[0]["image"].ToString());
+
+
+            return new Product(id, title, price, description, category, image);
+        }
+
+        [HttpGet("GetJSON")]
+        public string getJson()
+        {
+            return JsonConvert.SerializeObject(getAllProducts());
+        }
+
+
+      
+    }
+}
---
+++ csharp/WebApplication1/WebApplication1/Product.cs
@@ -0,0 +1,27 @@
+﻿namespace WebApplication1
+{
+    public class Product
+    {
+        public Product() { }
+
+        public Product(int id, string title, double price, string description,
+            string category, string image) { 
+            this.id = id;
+            this.title = title;
+            this.price = price;
+            this.description = description;      
+            this.category = category;
+            this.image = image;
+        }
+
+        public int id { get; set; }
+        public string title { get; set; }
+        public double price { get; set; }
+        public string description { get; set; }
+        public string category { get; set; }
+        public string image { get; set; }
+
+    }
+
+  }
+
